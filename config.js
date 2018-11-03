a29c54c6ada0bf9d91645b682cd8eafcb86f48be
skwarte <skwarte@purdue.edu>
2018-11-03 11:59:48 -0500

    set up twitter project and connected it to the twitter account

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  consumer_key: "pu9v0cxDTkz2PWLXLqrE8agg1",
+  consumer_secret: "ajRRpRh3N3lam0JGoRnIva84lXHTXTgTFylDUx55z4jxSZS3YJ",
+  access_token: "1058758595133784064-F4KQ7sprBiGPm7tK4ywPkwzUmDGjBv ",
+  access_token_secret: "u8znQiA4E2y5APbieRXOV5fFpdArupKfszIXF8Tf5OlgW"
+};
