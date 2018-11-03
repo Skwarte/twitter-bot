87122091f51703ceb0730efabdbef87e23733a66
skwarte <skwarte@purdue.edu>
2018-11-03 16:36:08 -0500

    schedule has been implemented

--- index.js
+++ index.js
@@ -3,14 +3,22 @@ var Twit = require("twit");
 var config = require("./config");
 
 var T = new Twit(config);
-var tweet = { status: "My first tweet from my app!" };
-T.post("statuses/update", tweet, tweeted);
 
-function tweeted(err, data, response) {
-  if (err) {
-    console.log("an error occured");
-  } else {
-    console.log("Tweet Sent");
+tweetIt();
+setInterval(tweetIt, 1000 * 20);
+
+function tweetIt() {
+  var r = Math.floor(Math.random() * 100);
+
+  var tweet = { status: r };
+  T.post("statuses/update", tweet, tweeted);
+
+  function tweeted(err, data, response) {
+    if (err) {
+      console.log("an error occured");
+    } else {
+      console.log("Tweet Sent");
+    }
   }
 }
 /** 
