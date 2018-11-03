b3e6b39552c66fa3d5f115ff11f4eea9ed44ee9e
skwarte <skwarte@purdue.edu>
2018-11-03 16:16:30 -0500

    integrated post method so now tweets can be sent out

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ var Twit = require("twit");
 var config = require("./config");
 
 var T = new Twit(config);
+var tweet = { status: "My first tweet from my app!" };
+T.post("statuses/update", tweet, tweeted);
+
+function tweeted(err, data, response) {
+  if (err) {
+    console.log("an error occured");
+  } else {
+    console.log("Tweet Sent");
+  }
+}
+/** 
 var params = { q: "Purdue", count: 10 };
 
 T.get("search/tweets", params, gotData);
@@ -10,3 +21,4 @@ T.get("search/tweets", params, gotData);
 function gotData(err, data, response) {
   console.log(data);
 }
+*/
